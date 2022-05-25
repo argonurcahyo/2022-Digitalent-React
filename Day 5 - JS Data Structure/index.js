0a77eaa80ec2d116a8b760042654dce6646fa6b1
Argo Nurcahyo <argonurcahyo25@gmail.com>
2022-05-25 20:04:15 +0700

    Day 5

---
+++ Day 5 - JS Data Structure/index.js
@@ -0,0 +1,32 @@
+// :: Array ::
+// const countries = ["Angola","Barbados","Chile","Djibouti","Estonia"];
+
+// console.log(countries.length);
+// console.log(countries[2]);
+
+// let nilai =[];
+// nilai[0] = 1000;
+// nilai[90] = 9000;
+
+// console.log(nilai);
+
+// :: Set ::
+// const seeet = new Set();
+// seeet.add(1);
+// seeet.add(4);
+// seeet.add(4);
+
+// console.log(seeet);
+
+// :: Map ::
+
+const maaap = new Map();
+maaap.set("Indonesia", "Jakarta");
+maaap.set("UK", "London");
+maaap.set("Brazil", "Brasilia");
+maaap.set("Argentina", "Rio de Janeiro");
+
+console.log(maaap.get("Indonesia"));
+console.log(maaap);
+
+console.log(maaap.values());
